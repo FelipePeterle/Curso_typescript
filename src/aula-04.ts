ef85753bb4311aa7d67723faaca1df97c25cf435
Felipe peterle <felipepeterle9@gmail.com>
2023-06-15 20:52:57 -0300

    edits on aula-04

--- src/aula-04.ts
+++ src/aula-04.ts
@@ -1,32 +1,30 @@
-interface Pessoas{
-    idade: number;
-    nome: string;
+interface Pessoas {
+  idade: number;
+  nome: string;
 }
 
-
-interface Usuarios{
-    pessoas: Pessoas[];
-    cidade: string;
+interface Usuarios {
+  pessoas: Pessoas[];
+  cidade: string;
 }
 
 const usuario: Usuarios = {
-    cidade: "Vila velha",
-    pessoas:[
+  cidade: "Vila velha",
+  pessoas: [
     {
-     idade: 50,
-     nome: "André",
-     },
-     {
-        idade: 25,
-        nome: "Julia",
-     },
-     {
-        idade: 31,
-        nome: "João",
-     },
-    ],
+      idade: 50,
+      nome: "André",
+    },
+    {
+      idade: 25,
+      nome: "Julia",
+    },
+    {
+      idade: 31,
+      nome: "João",
+    },
+  ],
 };
-
 
 console.log(usuario.pessoas.length);
 console.log(usuario);
