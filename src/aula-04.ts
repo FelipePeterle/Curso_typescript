a752c0d6f3de9e75f161a8a64da7442062549149
Felipe peterle <felipepeterle9@gmail.com>
2023-06-15 20:52:07 -0300

    feat: aula04

---
+++ src/aula-04.ts
@@ -0,0 +1,32 @@
+interface Pessoas{
+    idade: number;
+    nome: string;
+}
+
+
+interface Usuarios{
+    pessoas: Pessoas[];
+    cidade: string;
+}
+
+const usuario: Usuarios = {
+    cidade: "Vila velha",
+    pessoas:[
+    {
+     idade: 50,
+     nome: "André",
+     },
+     {
+        idade: 25,
+        nome: "Julia",
+     },
+     {
+        idade: 31,
+        nome: "João",
+     },
+    ],
+};
+
+
+console.log(usuario.pessoas.length);
+console.log(usuario);
