014db7ea15d77220be89bf9b5ea382d91d4068b3
Felipe peterle <felipepeterle9@gmail.com>
2023-06-15 21:06:19 -0300

    Edit on aula 05

--- src/aula-05.ts
+++ src/aula-05.ts
@@ -1,30 +1,29 @@
-interface Estado{
-    nome: string;
+interface Estado {
+  nome: string;
 }
 
-interface Cidade{
-    nome: string;
-    estado: Estado;
+interface Cidade {
+  nome: string;
+  estado: Estado;
 }
 
-
-interface Endereço{
-    rua: string;
-    bairro: string;
-    cidade: Cidade;
-    complemento?: string;
+interface Endereço {
+  rua: string;
+  bairro: string;
+  cidade: Cidade;
+  complemento?: string;
 }
 
 const endereco: Endereço = {
-    bairro: "Bairro",
-    rua: "rua qualquer",
-    cidade:{
-        nome: 'Rio',
-        estado: {
-            nome: "Rio",
-        },
+  bairro: "Bairro",
+  rua: "rua qualquer",
+  cidade: {
+    nome: "Rio",
+    estado: {
+      nome: "Rio",
     },
+  },
 };
 
-console.log(endereco.complemento?.substring(3) || 'DEU ERRO');
+console.log(endereco.complemento?.substring(3) || "DEU ERRO");
 console.log(endereco);
