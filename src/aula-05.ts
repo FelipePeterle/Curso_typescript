bc265dc9acfb42ea8301ef9c72289dd9d8317b28
Felipe peterle <felipepeterle9@gmail.com>
2023-06-15 21:05:39 -0300

    feat: aula-05

---
+++ src/aula-05.ts
@@ -0,0 +1,30 @@
+interface Estado{
+    nome: string;
+}
+
+interface Cidade{
+    nome: string;
+    estado: Estado;
+}
+
+
+interface Endereço{
+    rua: string;
+    bairro: string;
+    cidade: Cidade;
+    complemento?: string;
+}
+
+const endereco: Endereço = {
+    bairro: "Bairro",
+    rua: "rua qualquer",
+    cidade:{
+        nome: 'Rio',
+        estado: {
+            nome: "Rio",
+        },
+    },
+};
+
+console.log(endereco.complemento?.substring(3) || 'DEU ERRO');
+console.log(endereco);
