496fc9044457f6c5691595d4f8c52e6989b97881
Felipe peterle <felipepeterle9@gmail.com>
2023-06-15 21:17:24 -0300

    Changes on aula 6

--- src/aula-06.ts
+++ src/aula-06.ts
@@ -1,17 +1,17 @@
 interface ObjetoQualquer {
-    funcao: (param1: number, param2?: number) => number;
-};
+  funcao: (param1: number, param2?: number) => number;
+}
 
-const funcaoTeste = (param1: number, param2?: number): number =>{
-    if (!param2){
-        return param1;
-    }
+const funcaoTeste = (param1: number, param2?: number): number => {
+  if (!param2) {
+    return param1;
+  }
 
-    return param1 + param2;
+  return param1 + param2;
 };
 
 const objetoQualquer: ObjetoQualquer = {
-    funcao: funcaoTeste,
+  funcao: funcaoTeste,
 };
 
 console.log(objetoQualquer.funcao(1));
