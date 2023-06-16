51ff9e6eff05cf9efff5f0e05c4dedbe41580862
Felipe peterle <felipepeterle9@gmail.com>
2023-06-15 21:34:49 -0300

    changes on aula 7

--- src/aula-07.ts
+++ src/aula-07.ts
@@ -1,19 +1,15 @@
 const funcaoCallback = (valor: number): void => {
-    console.log("Deu bom " + valor)
-}
+  console.log("Deu bom " + valor);
+};
 
-
-const funcaoTeste2 = (
-    valor1: number, 
-    valor2: number, 
-    callback: (n: number) => void): string =>{
-    if (valor1 > valor2){
-        callback(valor1);
-    }
-    return"";
+const funcaoTeste2 = (valor1: number, valor2: number, callback: (n: number) => void): string => {
+  if (valor1 > valor2) {
+    callback(valor1);
+  }
+  return "";
 };
 
 funcaoTeste2(522, 32, funcaoCallback);
-funcaoTeste2(530, 32, (x: number) =>{
-    console.log("Outra função " + x)
+funcaoTeste2(530, 32, (x: number) => {
+  console.log("Outra função " + x);
 });
